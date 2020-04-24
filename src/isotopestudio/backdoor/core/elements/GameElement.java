d7dfa6b6d25492d608757f0a95ceb290b64cecb7
BESSIERE Doryan <BESSIERE Doryan@DESKTOP-AHBEF37>
2020-04-24 14:31:06 +0200

    Self save

--- src/isotopestudio/backdoor/core/elements/GameElement.java
+++ src/isotopestudio/backdoor/core/elements/GameElement.java
@@ -13,16 +13,13 @@ import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import doryanbessiere.isotopestudio.commons.GsonInstance;
 import isotopestudio.backdoor.core.team.Team;
-import isotopestudio.backdoor.game.utils.country.Country;
-import isotopestudio.backdoor.network.packet.packets.PacketSendElementData;
-import isotopestudio.backdoor.network.player.NetworkedPlayer;
-import isotopestudio.backdoor.network.server.GameServer;
+import isotopestudio.backdoor.core.utils.Country;
 
 public class GameElement {
 
-	public static Map<String, GameElement> gameElements = Collections.synchronizedMap(new LinkedHashMap<>());
+	public static final Map<String, GameElement> CACHE = Collections.synchronizedMap(new LinkedHashMap<>());
+	
 	private String name;
 	private UUID uuid;
 	private static ArrayList<String> adress_used = new ArrayList<String>();
@@ -35,7 +32,6 @@ public class GameElement {
 	private GameElementType type;
 
 	private HashMap<Team, Integer> points = new HashMap<Team, Integer>();
-	private transient ArrayList<NetworkedPlayer> connected = new ArrayList<NetworkedPlayer>();
 	private ArrayList<Team> team_connected = new ArrayList<Team>();
 	private int max_points = 1;
 
@@ -68,37 +64,11 @@ public class GameElement {
 		if (team != null)
 			linked = true;
 
-		gameElements.put(uuid.toString(), this);
-	}
-
-	public void connect(NetworkedPlayer player) {
-		connected.add(player);
-		if (!getTeamConnected().contains(player.getTeam()))
-			getTeamConnected().add(player.getTeam());
-		GameServer.gameServer.sendAll(new PacketSendElementData(this));
-		if (getType() == GameElementType.SERVER)
-			return;
-		player.removeMoney(50);
-	}
-
-	public void disconnect(NetworkedPlayer player) {
-		connected.remove(player);
-		getTeamConnected().remove(player.getTeam());
-		for (NetworkedPlayer player_ : getConnected()) {
-			if (player_.getTeam() == player.getTeam()) {
-				getTeamConnected().add(player.getTeam());
-				break;
-			}
-		}
-		GameServer.gameServer.sendAll(new PacketSendElementData(this));
+		CACHE.put(getUUID().toString(), this);
 	}
 
 	public boolean isTeamConnected(Team team) {
 		return getTeamConnected().contains(team);
-	}
-
-	public ArrayList<NetworkedPlayer> getConnected() {
-		return connected;
 	}
 
 	public GameElementType getType() {
@@ -128,138 +98,11 @@ public class GameElement {
 	public Integer getTeamPoint(Team team) {
 		return points.get(team);
 	}
-
-	public void attack(Team team) {
-		if (getType() == GameElementType.SERVER) {
-			if (team == getTeam()) {
-				int point = getTeamPoint(getTeam()) + 1;
-				setTeamPoint(getTeam(), point > max_points ? max_points : point);
-			} else {
-				int point = getTeamPoint(getTeam()) - 1;
-				setTeamPoint(getTeam(), point < 0 ? 0 : point);
-			}
-
-			if (getTeamPoint(getTeam()) <= 0) {
-				GameServer.gameServer.getParty().lose(getTeam());
-				GameServer.gameServer.getParty().stop();
-			}
-		} else if (getType() == GameElementType.NODE) {
-			int point = getTeamPoint(team) + 1;
-			point = point > max_points ? max_points : point;
-			point = point < 0 ? 0 : point;
-			
-			setTeamPoint(team, point);
-
-			/**
-			 * Quand le noeud est neutre on rajoute un point à chaque joueur
-			 * 
-			 * Quand le noeud appartient à une équipe on fait un système de duel: quand la
-			 * team qui a le serveur attack enlève un point à l'adversaire
-			 * 
-			 */
-
-			if (getTeam() != null) {
-				if(getTeam() == team) // L'équipe possède déjà le serveur
-					return;
-				for (Team team_ : Team.values()) {
-					if (team_ != team) {
-						int team_point = getTeamPoint(team_) - 1;
-						if (team_point < 0)
-							team_point = 0;
-						setTeamPoint(team_, team_point);
-					}
-				}
-			}
-
-			if (getTeamPoint(team) >= max_points) {
-				if (getTeam() == null) {
-					setTeam(team);
-					for (Team team_ : Team.values()) {
-						if (team_ != team) {
-							setTeamPoint(team_, 0);
-						} else {
-							setTeamPoint(team_, max_points);
-						}
-					}
-					linked = true;
-					/*
-					 * - On récupère tous les noeuds de l'équipe
-					 * - On vérifie si le noeud est défini comme 'linked'
-					 * - Si ce n'est pas le cas alors qu'il est désormais 'linked' on lui défini sa variable et on le met à jours pour
-					 * les joueurs
-					 */
-					for (GameElement node : GameServer.gameServer.getParty().getNodes(getTeam())) {
-						if (!node.linked && GameServer.gameServer.getParty().isLinked(node, getTeam())) {
-							node.linked = true;
-							GameServer.gameServer.sendAll(new PacketSendElementData(node));
-						}
-					}
-				} else {
-					if (getTeam() == team)
-						return;
-
-					for (Team team_ : Team.values()) {
-						setTeamPoint(team_, 0);
-					}
-					linked = false;
-
-					Team old_team = getTeam();
-
-					setTeam(null);
-
-					/*
-					 * 
-					 * - On récupère tous les noeuds de l'équipe
-					 * - On vérifie si le noeud n'est pas 'linked' on lui défini sa variable et on envoie l'informations aux joueurs
-					 * et on déconnecte l'équipe 
-					 */
-					for (GameElement node : GameServer.gameServer.getParty().getNodes(old_team)) {
-						if (!GameServer.gameServer.getParty().isLinked(node, old_team)) {
-							node.linked = false;
-							GameServer.gameServer.sendAll(new PacketSendElementData(node));
-							node.disconnectTeam(old_team);
-						}
-					}
-
-					/*
-					 * - On récupère les joueurs de l'ancienne équipe
-					 * - On vérifie si ils sont connectés sur une noeuds/serveurs et si il n'est plus 'linked'
-					 * - On déconnecte l'équipe
-					 */
-					ArrayList<NetworkedPlayer> players = new ArrayList<NetworkedPlayer>();
-					players.addAll(old_team.getPlayers());
-					for (NetworkedPlayer player : players) {
-						if (player.getTargetAddress() != null) {
-							GameElement node = GameServer.gameServer.getParty().getNodeByAddress(player.getTargetAddress());
-							if (/*node.getTeam() != player.getTeam() && */!GameServer.gameServer.getParty().isLinked(node, old_team)) {
-								node.disconnect(player);
-							}
-						}
-					}
-				}
-				disconnectAll();
-			}
-		}
-		GameServer.gameServer.sendAll(new PacketSendElementData(this));
+	
+	public void setLinked(boolean linked) {
+		this.linked = linked;
 	}
-
-	public void disconnectAll() {
-		ArrayList<NetworkedPlayer> connected = new ArrayList<NetworkedPlayer>();
-		connected.addAll(getConnected());
-		for (NetworkedPlayer player : connected) {
-			GameServer.gameServer.getParty().disconnect(player);
-		}
-	}
-
-	public void disconnectTeam(Team team) {
-		ArrayList<NetworkedPlayer> connected = new ArrayList<NetworkedPlayer>();
-		for (NetworkedPlayer player : connected) {
-			if (player.getTeam() == team) {
-				GameServer.gameServer.getParty().disconnect(player);	
-			}
-		}
-	}
-
+	
 	public int getMaxPoints() {
 		return max_points;
 	}
@@ -331,5 +174,9 @@ public class GameElement {
 				return (f.getDeclaringClass() == GameElement.class && f.getName().equals("connected"));
 			}
 		}).create() : gson;
+	}
+
+	public static GameElement getGameElement(String uuid) {
+		return CACHE.get(uuid);
 	}
 }
--- src/isotopestudio/backdoor/core/team/Team.java
+++ src/isotopestudio/backdoor/core/team/Team.java
@@ -2,62 +2,24 @@ package isotopestudio.backdoor.core.team;
 
 import java.util.ArrayList;
 
-import isotopestudio.backdoor.network.player.NetworkedPlayer;
-import isotopestudio.backdoor.network.server.GameServer;
-
 public enum Team {
 
-	RED("team_red", 1), 
-	BLUE("team_blue", 1);
+	RED("team_red"), 
+	BLUE("team_blue");
 
 	private String path;
-	private int max_players;
 
-	private Team(String path, int max_players) {
+	private Team(String path) {
 		this.path = path;
-		this.max_players = max_players;
 	}
 
 	public String getPath() {
 		return path;
 	}
 
-	private ArrayList<NetworkedPlayer> players = new ArrayList<>();
-
-	public void join(NetworkedPlayer player) {
-		players.add(player);
-	}
-
-	public void leave(NetworkedPlayer player) {
-		players.remove(player);
-		if (GameServer.gameServer.getParty() != null && GameServer.gameServer.getParty().isStarted()) {
-			GameServer.gameServer.getParty().lose(this);
-			GameServer.gameServer.stopParty();
-		}
-	}
-
-	public int getMaxPlayers() {
-		return max_players;
-	}
-
-	public ArrayList<NetworkedPlayer> getPlayers() {
-		return players;
-	}
-
 	public static Team get(String teamString) {
 		for (Team team : Team.values()) {
 			if (team.toString().equalsIgnoreCase(teamString)) {
-				return team;
-			}
-		}
-		return null;
-	}
-
-	public static Team joinAuto(NetworkedPlayer player) {
-		for (Team team : Team.values()) {
-			if (team.getPlayers().size() < team.getMaxPlayers()) {
-				team.join(player);
-				System.out.println(player.getUsername()+" has join -> "+player.getTeam());
 				return team;
 			}
 		}
